ee1c13cb8846afe9c181ad575f770ce8743f2ed8
rtejerina <r.tejerinazap@outlook.es>
2025-06-24 21:27:52 +0200

    Conflicto merge con el controlador

--- src/main/java/com/cursos/controller/CursosController.java
+++ src/main/java/com/cursos/controller/CursosController.java
@@ -37,6 +37,13 @@ public class CursosController {
 		return "curso"; // Redirige a la vista del curso jsp
 	}
 	
+	// Movido desde abajo para probar conflicto
+		@GetMapping("/eliminar")
+		public String eliminarCurso(@RequestParam("idCurso") int idCurso) {
+			service.eliminarCurso(idCurso);
+			return "menuPrincipal"; 
+		}
+	
 	@GetMapping("/recuperarActualizar")
 	public String recuperarActualizarCurso(@RequestParam("idCurso") int idCurso, HttpServletRequest request) {
 		Curso curso = service.buscarCurso(idCurso);
@@ -52,11 +59,6 @@ public class CursosController {
 		return "menu";
 	}
 	
-	// Movido desde arriba para probar conflicto
-	@GetMapping("/eliminar")
-	public String eliminarCurso(@RequestParam("idCurso") int idCurso) {
-		service.eliminarCurso(idCurso);
-		return "menu"; 
-	}
+	
 	
 }
